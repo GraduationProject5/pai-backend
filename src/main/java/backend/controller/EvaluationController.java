d102b51a1343e7edb68d0bebee0ee3d330f23411
chinalienming <728385437@qq.com>
2019-03-25 21:39:19 +0800

    评估和部分机器学习组件的算法接口,以及debugController传参Service

--- src/main/java/backend/controller/EvaluationController.java
+++ src/main/java/backend/controller/EvaluationController.java
@@ -1,4 +1,47 @@
 package backend.controller;
 
+import backend.service.DataService;
+import backend.service.EvaluationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
+
+@RestController
+@RequestMapping(value = "/evaluation")
 public class EvaluationController {
+
+    @Autowired
+    EvaluationService evaluationService;
+    @Autowired
+    DataService dataService;
+
+    @GetMapping(value = "/cluster_evaluation")
+    public Map cluster(
+            @RequestBody Map<String, Object> map
+    ) {
+        return evaluationService.cluster_evaluation(map);
+    }
+
+    @GetMapping(value = "/re")
+    public void re(){
+
+    }
+
+    @GetMapping(value = "/tcd")
+    public void tcd(){
+
+    }
+
+    @GetMapping(value = "/mcd")
+    public void mcd(){
+
+    }
+
+    @GetMapping(value = "/confusion_matrix")
+    public void confusionMatrix(){
+
+    }
+
+
 }
