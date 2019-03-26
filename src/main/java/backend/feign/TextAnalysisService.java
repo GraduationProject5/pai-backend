8b516f18b89113ac7cf1b5be28bdc76eb736982f
chenpipin <gmdchanbb@gmail.com>
2019-03-26 16:07:54 +0800

    commit text analysis

--- src/main/java/backend/feign/TextAnalysisService.java
+++ src/main/java/backend/feign/TextAnalysisService.java
@@ -1,7 +1,6 @@
 package backend.feign;
 
 import backend.service.DataService;
-import backend.feign.TextAnalysisFeign;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.*;
@@ -11,7 +10,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/text-analysis")
+@RequestMapping(value = "/text-analysis")
 //暂时保留 RequestMapping 用来测试算法能否调用
 @Service
 public class TextAnalysisService {
@@ -21,13 +20,33 @@ public class TextAnalysisService {
     @Autowired
     private DataService dataService;
 
-    @RequestMapping(value = "/participles")
+    /**
+     * @param userID
+     * @param tableName
+     * @return
+     */
+    @GetMapping(value = "getParticiples")
     public Map getParticiples(@SessionAttribute("userID") String userID,
                               @RequestParam("tableName") String tableName) {
         //获取表中的row
         List tableList = dataService.getData(Long.parseLong(userID), tableName);
-        Map mapText = new HashMap<>();
+        Map<String, Object> mapText = new HashMap<>();
         mapText.put("text", ((Map) tableList.get(0)).get("text"));
-        return textAnalysisFeign.getParticiples(mapText);
+        return textAnalysisFeign.participles(mapText);
+    }
+
+    @GetMapping(value = "getStopwordsFilter")
+    public Map getStopwordsFilter(@RequestBody Map<String, Object> map) {
+        return textAnalysisFeign.stopwords_filter(map);
+    }
+
+    @GetMapping(value = "getFrequencyStatistics")
+    public Map getFrequencyStatistics(@RequestBody Map<String, Object> map) {
+        return textAnalysisFeign.frequency_statistics(map);
+    }
+
+    @GetMapping(value = "getLDA")
+    public Map getLDA(@RequestBody Map<String, Object> map) {
+        return textAnalysisFeign.LDA(map);
     }
 }
